4319903a58cfc26fbad6120160ed0b96942e4eba
Gia Bao <nggbaobkit@gmail.com>
2020-05-13 10:33:54 +0700

    Updates

---
+++ precache-manifest.02f4586dab8f55e38518dadb2c4ade7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fdac0c2a53dac1ecbf9bbcdca37c4fb",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "c6e4407b2ddd0d92b168",
+    "url": "/covid-19-tracker/static/css/main.1fb4fe65.chunk.css"
+  },
+  {
+    "revision": "1951ba79ef1dc69dd2eb",
+    "url": "/covid-19-tracker/static/js/2.934bf90b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.934bf90b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6e4407b2ddd0d92b168",
+    "url": "/covid-19-tracker/static/js/main.2ddc9daf.chunk.js"
+  },
+  {
+    "revision": "fa0c104a82072024f0ae",
+    "url": "/covid-19-tracker/static/js/runtime-main.5b571555.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
